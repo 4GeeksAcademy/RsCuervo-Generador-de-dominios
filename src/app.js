6e0cd2f7a24752fcadec901e64ac1ad06e2254c4
Richard Cuervo <richardcuer@hotmail.com>
2025-02-10 23:34:53 +0000

    first commit

--- src/app.js
+++ src/app.js
@@ -1,11 +1,15 @@
-import "bootstrap";
-import "./style.css";
+let pronoun = ['the', 'our'];
+let adj = ['great', 'big'];
+let noun = ['jogger', 'racoon'];
+let extension = ['.com', '.net', '.us', '.io' ]
+for (let i = 0; i < pronoun.length; i++) {
+  for (let a = 0; a < adj.length; a++) {
+    for (let n = 0; n < noun.length; n++) {
+      for(let e=0; e<extension.length; e++){
+        console.log(pronoun[i] + adj[a] + noun[n] + extension[e]);
+      }
+      
+    }
+  }
+}
 
-
-import "./assets/img/rigo-baby.jpg";
-import "./assets/img/4geeks.ico";
-
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
